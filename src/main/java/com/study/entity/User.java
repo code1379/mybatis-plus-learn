8c4c7cb4a2d6d62e6f53b52c5994e6a8e4ad9c2c
guojingzhou <guojingzhou@huayun.com>
2022-07-08 17:37:12 +0800

    feat: mybatis-plus 常用注解 @TableName @TableId @TableField

--- src/main/java/com/study/entity/User.java
+++ src/main/java/com/study/entity/User.java
@@ -1,15 +1,28 @@
 package com.study.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
+// @TableName("mp_user") // 指定表名为 mp_user
 public class User {
+    /*
+    * 1. MP 默认找 id 作为主键。如果数据库中的 id 改为 user_id，那么我们直接将 这里的 id 改为 userId，
+    * 之后执行插入语句是不生效的。因为 MP 不知道主键是那个了。 可以通过 @TableId 来指定主键
+    * 2. 有的公司因为某些原因，实体类 和 数据表没有对应关系！！！
+    * 比如 表中叫 name，实体中叫 realname。 或者说 我就比较任性，我就想让他们不一样。
+    * 可以通过 @TableField('name') 来指定表中对应的 Field
+    * */
+    // @TableId // 设置主键
     private Long id;
+    // @TableField("name") // 设置表中对应的 Field
     private String name;
     private Integer age;
     private String email;
-    private Long manager_id;
-    private Date create_time;
+    private Long managerId;
+    private Date createTime;
 }
--- src/test/java/com/study/InsertTest.java
+++ src/test/java/com/study/InsertTest.java
@@ -14,12 +14,17 @@ public class InsertTest {
 
     @Test
     public void insert(){
+        /*
+        * 1. id 不设置，默认使用雪花算法自增id
+        * 2. 实体类字段驼峰，可以对应到数据库里面 _ ， managerId => manager_id
+        * 3. 实体类 User 可以对应到 数据库的 user(User) 表。也可以驼峰转带下划线的
+        * */
        User user = new User();
-       user.setName("UI打工仔");
+       user.setName("富二代打工仔");
        user.setAge(18);
-       user.setEmail("ui@study.com");
-       user.setId(Long.valueOf(6));
-       user.setManager_id(Long.valueOf(2));
+       user.setEmail("rich@study.com");
+       // user.setId(Long.valueOf(7)); // 不设置 id，MP 会根据雪花算法自增 id
+       user.setManagerId(Long.valueOf(2)); // 这里的 ManagerId 是驼峰，数据库表里是 manager_id
        int rows = userMapper.insert(user);
         System.out.println("影响记录数:" + rows);
     }
