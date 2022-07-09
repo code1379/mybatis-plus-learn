f874b76736c3ced3760986cb2f20f1889b458b0a
guojingzhou <guojingzhou@huayun.com>
2022-07-09 11:30:18 +0800

    feat: 3-2 条件构造器查询（1） QueryWrapper 继承自 AbstractWrapper

--- src/test/java/com/study/RetrieveTest.java
+++ src/test/java/com/study/RetrieveTest.java
@@ -1,5 +1,6 @@
 package com.study;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.study.entity.User;
 import com.study.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
@@ -49,4 +50,46 @@ public class RetrieveTest {
         userList.forEach(System.out::println);
     }
 
+    /*
+    * 下面都是条件查询构造器 Wrapper
+    * 双击 Shift 查询 AbstractWrapper。QueryWrapper 是 AbstractWrapper 的子类
+    * */
+    @Test
+    public void selectListByWrapper(){
+        // 名字中包含 打工仔 且 年龄小于 40
+        // name like "%打工仔%" and age < 40
+        // SELECT id,name,age,email,manager_id,create_time FROM user WHERE (name LIKE ? AND age < ?)
+        // Parameters: %打工仔%(String), 40(Integer)
+
+        // * QueryWrapper 是 AbstractWrapper 的一个子类
+        QueryWrapper<User> queryWrapper = new QueryWrapper<User>();
+        queryWrapper.like("name", "打工仔").lt("age", 40);
+        List<User> userList = userMapper.selectList(queryWrapper);
+        userList.forEach(System.out::println);
+    }
+
+
+    @Test
+    public void selectListByWrapper2(){
+        // 名字中包含 打工仔 且 （年龄大于等于 20 且 小于等于 40）并且 email 不为空
+        // name like "%打工仔%" and age between 20 and 40 and email is not null
+        // SELECT id,name,age,email,manager_id,create_time FROM user WHERE (name LIKE ? AND age BETWEEN ? AND ? AND email IS NOT NULL)
+        // %打工仔%(String), 20(Integer), 40(Integer)
+        QueryWrapper<User> queryWrapper = new QueryWrapper<User>();
+        queryWrapper.like("name", "打工仔").between("age", 20, 40).isNotNull("email");
+        List<User> userList = userMapper.selectList(queryWrapper);
+        userList.forEach(System.out::println);
+    }
+
+    @Test
+    public void selectListByWrapper3(){
+        // 名字中为王姓 *或者* 年龄大于等于 40，按照年龄降序排列，年龄相同的按照 id 升序排列
+
+        // SELECT id,name,age,email,manager_id,create_time FROM user WHERE (name LIKE ? AND age >= ?) ORDER BY age DESC,id ASC
+        // %王%%(String), 40(Integer)
+        QueryWrapper<User> queryWrapper = new QueryWrapper<User>();
+        queryWrapper.likeRight("name", "王").or().ge("age", 40).orderByDesc("age").orderByAsc("id");
+        List<User> userList = userMapper.selectList(queryWrapper);
+        userList.forEach(System.out::println);
+    }
 }
